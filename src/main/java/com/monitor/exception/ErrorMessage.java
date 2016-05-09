9e3aafc5c8c5b2d1edfcebea1c3cfbef9f980ed9
lrcUnlimited <lirunchao@126.com>
2016-05-09 11:02:34 +0800

    错误信息

---
+++ src/main/java/com/monitor/exception/ErrorMessage.java
@@ -0,0 +1,19 @@
+package com.monitor.exception;
+
+public class ErrorMessage {
+	private String message;
+
+	public ErrorMessage(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
