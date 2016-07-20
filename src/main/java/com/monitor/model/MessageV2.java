c9223c88fa89e871b902ee84d9cd555b65af1bea
lrcUnlimited <lirunchao@126.com>
2016-07-20 17:13:10 +0800

    新消息model

---
+++ src/main/java/com/monitor/model/MessageV2.java
@@ -0,0 +1,55 @@
+package com.monitor.model;
+
+import java.util.Date;
+
+/**
+ * socket接受客户端消息类
+ * 
+ * @author AlanJager
+ * 
+ */
+public class MessageV2 {
+	private int deviceId;// 被控端id
+	private String MachineID;// 会话秘钥生成时间
+	private double longitude;// 经度
+	private double latitude;// 纬度
+	private int dwtype; //暂不适用1:心跳 2:注册
+	private int type;// 发送命令 0-代表关机 1-代表开机
+
+	public int getDeviceId() {
+		return deviceId;
+	}
+	public void setDeviceId(int deviceId) {
+		this.deviceId = deviceId;
+	}
+	public String getMachineID() {
+		return MachineID;
+	}
+	public void setMachineID(String machineID) {
+		MachineID = machineID;
+	}
+	public double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	public double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+	public int getDwtype() {
+		return dwtype;
+	}
+	public void setDwtype(int dwtype) {
+		this.dwtype = dwtype;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+}
---
+++ src/main/java/com/monitor/model/SendMessageV2.java
@@ -0,0 +1,33 @@
+package com.monitor.model;
+
+public class SendMessageV2 {
+	private int dwtype; //暂不使用 1:心跳, 2:注册
+	private int TurnOnOff;// 0:关机 ，1：开机
+	private String MachineID;
+	private int UpdateSystem; //0：不更新受控设备程序 1:更新受控设备程序
+	
+	public int getDwtype() {
+		return dwtype;
+	}
+	public void setDwtype(int dwtype) {
+		this.dwtype = dwtype;
+	}
+	public int getTurnOnOff() {
+		return TurnOnOff;
+	}
+	public void setTurnOnOff(int turnOnOff) {
+		TurnOnOff = turnOnOff;
+	}
+	public String getMachineID() {
+		return MachineID;
+	}
+	public void setMachineID(String machineID) {
+		MachineID = machineID;
+	}
+	public int getUpdateSystem() {
+		return UpdateSystem;
+	}
+	public void setUpdateSystem(int updateSystem) {
+		UpdateSystem = updateSystem;
+	}
+}
