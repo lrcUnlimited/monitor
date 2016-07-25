9351c1d434efb11718bbadefbedf55e43e39e6fb
ZyanZou <873863981@qq.com>
2016-07-25 15:04:01 +0800

    download file

--- src/main/java/com/monitor/controller/DownloadAction.java
+++ src/main/java/com/monitor/controller/DownloadAction.java
@@ -1,51 +1,93 @@
 package com.monitor.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
-
-/**
- * Created by zouye on 16/7/25.
- */
+import java.net.URLConnection;
+import java.nio.charset.Charset;
 
 @Controller
-@RequestMapping("/file")
 public class DownloadAction {
-    @RequestMapping("/download")
-    public String download(String fileName, HttpServletRequest request,
-                           HttpServletResponse response) {
-        response.setCharacterEncoding("utf-8");
-        response.setContentType("multipart/form-data");
-        response.setHeader("Content-Disposition", "attachment;fileName="
-                + fileName);
-        try {
-            String path = Thread.currentThread().getContextClassLoader()
-                    .getResource("").getPath()
-                    + "download";//这个download目录为啥建立在classes下的
-            InputStream inputStream = new FileInputStream(new File(path
-                    + File.separator + fileName));
+    private static final String INTERNAL_FILE = "irregular-verbs-list.pdf";
+    private static final String EXTERNAL_FILE_PATH = "C:/mytemp/SpringMVCHibernateManyToManyCRUDExample.zip";
 
-            OutputStream os = response.getOutputStream();
-            byte[] b = new byte[2048];
-            int length;
-            while ((length = inputStream.read(b)) > 0) {
-                os.write(b, 0, length);
-            }
+    /*
+     * Download a file from
+     *   - inside project, located in resources folder.
+     *   - outside project, located in File system somewhere.
+     */
+    @RequestMapping(value = "/download/{type}", method = RequestMethod.GET)
+    public void downloadFile(HttpServletResponse response, @PathVariable("type") String type) throws IOException {
 
-            // 这里主要关闭。
-            os.close();
+        File file = null;
 
-            inputStream.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (type.equalsIgnoreCase("internal")) {
+            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+            file = new File(classloader.getResource(INTERNAL_FILE).getFile());
+        } else {
+            file = new File(EXTERNAL_FILE_PATH);
         }
-        //  返回值要注意，要不然就出现下面这句错误！
-        //java+getOutputStream() has already been called for this response
-        return null;
+
+        if (!file.exists()) {
+            String errorMessage = "Sorry. The file you are looking for does not exist";
+            System.out.println(errorMessage);
+            OutputStream outputStream = response.getOutputStream();
+            outputStream.write(errorMessage.getBytes(Charset.forName("UTF-8")));
+            outputStream.close();
+            return;
+        }
+
+        String mimeType = URLConnection.guessContentTypeFromName(file.getName());
+        if (mimeType == null) {
+            System.out.println("mimetype is not detectable, will take default");
+            mimeType = "application/octet-stream";
+        }
+
+        System.out.println("mimetype : " + mimeType);
+
+        response.setContentType(mimeType);
+
+        /* "Content-Disposition : inline" will show viewable types [like images/text/pdf/anything viewable by browser] right on browser
+            while others(zip e.g) will be directly downloaded [may provide save as popup, based on your browser setting.]*/
+        response.setHeader("Content-Disposition", String.format("inline; filename=\"" + file.getName() + "\""));
+
+
+        /* "Content-Disposition : attachment" will be directly download, may provide save as popup, based on your browser setting*/
+        //response.setHeader("Content-Disposition", String.format("attachment; filename=\"%s\"", file.getName()));
+
+        response.setContentLength((int) file.length());
+
+        InputStream inputStream = new BufferedInputStream(new FileInputStream(file));
+
+        //Copy bytes from source to destination(outputstream in this example), closes both streams.
+        FileCopyUtils.copy(inputStream, response.getOutputStream());
+    }
+
+    @RequestMapping(value = "/downloading", method = RequestMethod.GET)
+    public void download(HttpServletResponse response) throws IOException {
+
+        File file = new File("C:/files/test.txt");
+        InputStream is = new FileInputStream(file);
+
+        // MIME type of the file
+        response.setContentType("application/octet-stream");
+        // Response header
+        response.setHeader("Content-Disposition", "attachment; filename=\""
+                + file.getName() + "\"");
+        // Read from the file and write into the response
+        OutputStream os = response.getOutputStream();
+        byte[] buffer = new byte[1024];
+        int len;
+        while ((len = is.read(buffer)) != -1) {
+            os.write(buffer, 0, len);
+        }
+        os.flush();
+        os.close();
+        is.close();
     }
 }
